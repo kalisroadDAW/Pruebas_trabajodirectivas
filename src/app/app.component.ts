c9ee5fa5826a9dd9dc4fc43af6ab917570689a02
Javier <javierghdaw@gmail.com>
2022-03-24 15:46:11 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'trabajodirectivas';
+  title = 'formulario de registro';
+  mensaje="";
+  registrado=false;
+  nombre:string="";
+  apellido:string="";
+  entradas=new Array();
+
+  constructor(){
+
+    this.entradas=[
+      
+      {titulo:"Python cada día más presente"},
+      {titulo:"js cada día más presente"},
+      {titulo:"ruby cada día más presente"},
+      {titulo:"angular cada día más presente"},
+      {titulo:"raul cada día más presente"},
+      {titulo:"periquin cada día más presente"},
+      {titulo:"jose cada día más presente"},
+    ]
+  }
+
+  registrarUsuario(){
+
+     this.registrado=true;
+
+     this.mensaje="usuario registrado";
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,6 @@
+import { Template } from '@angular/compiler/src/render3/r3_ast';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -10,7 +12,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
